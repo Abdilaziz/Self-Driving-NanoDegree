6679bc429f76e64d04cead474a7ff2870d760869
Abdi <azizib2nd@gmail.com>
2017-02-27 20:08:44 -0500

    Completed Lane Lines Project Results

--- Term 1/Lane Lines Project/projCode.py
+++ Term 1/Lane Lines Project/projCode.py
@@ -37,27 +37,11 @@ def process_image(image):
 
 	# This time we are defining a four sided polygon to mask
 	imshape = image.shape
-	vertices = np.array([[(0+130,imshape[0]),(460, 315), (500, 315), (imshape[1]-60,imshape[0])]], dtype=np.int32)
+	# vertices = np.array([[(0+130,imshape[0]),(460, 315), (500, 315), (imshape[1]-60,imshape[0])]], dtype=np.int32)
+	vertices = np.array([[(0,imshape[0]),(475, 310), (465, 310), (imshape[1],imshape[0])]], dtype=np.int32)
+
 	cv2.fillPoly(mask, vertices, ignore_mask_color)
 	masked_edges = cv2.bitwise_and(edges, mask)
-
-
-	# # Next we'll create a masked edges image using cv2.fillPoly()
-	# mask = np.zeros_like(color_masked)
-	# ignore_mask_color = 255
-
-	# # This time we are defining a four sided polygon to mask
-	# imshape = image.shape
-	# vertices = np.array([[(0,imshape[0]),(450, 290), (490, 290), (imshape[1],imshape[0])]], dtype=np.int32)
-	# cv2.fillPoly(mask, vertices, ignore_mask_color)
-	# masked_image = cv2.bitwise_and(color_masked, mask)
-
-	# # Define our parameters for Canny and apply
-	# low_threshold = 130
-	# high_threshold = 150
-	# masked_edges = cv2.Canny(masked_image, low_threshold, high_threshold)
-
-
 
 
 	# Define the Hough transform parameters
@@ -78,85 +62,100 @@ def process_image(image):
 	leftSlopeTotal = 0
 	rightSlopeTotal = 0
 
-	leftSlopeAmount = 0
-	rightSlopeAmount = 0
+	leftYintTotal = 0
+	rightYintTotal = 0
 
-	rightMaxPoint = 0
-	rightMinPoint = 0
-
-	leftMaxPoint = 0
-	leftMinPoint = 0
-
+	leftAmount = 0
+	rightAmount = 0
 
 	for line in lines:
 	    for x1,y1,x2,y2 in line:
+
 	    	if((x2-x1)==0):
 	    		print('One of the values divides by 0')
 	    	else:
 	    		curSlope = (y2-y1)/(x2-x1)
+	    		curYint1 = -1*curSlope*x1 + y1
+	    		curYint2 = -1*curSlope*x2 + y2
 		    	if(curSlope<=-0.5):
 		    		# Left Side
 		    		leftSlopeTotal = leftSlopeTotal + curSlope
-		    		leftSlopeAmount = leftSlopeAmount + 1
-		    		cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10) 
+		    		leftYintTotal = leftYintTotal + curYint1
+		    		leftAmount = leftAmount + 1
+		    		
 		    	elif(curSlope>=0.5):
 		    		# Right Side
 		    		rightSlopeTotal = rightSlopeTotal + curSlope
-		    		rightSlopeAmount= rightSlopeAmount + 1
-		    		cv2.line(line_image,(x1,y1),(x2,y2),(0,0,255),10)
+		    		rightYintTotal = rightYintTotal + curYint1
+		    		rightAmount= rightAmount + 1
 
-	avgLeftSlope = leftSlopeTotal/leftSlopeAmount
-	avgRightSlope = rightSlopeTotal/rightSlopeAmount
-	print(avgLeftSlope)
-	print(avgRightSlope)
+	avgLeftSlope =leftSlopeTotal/leftAmount
+	avgRightSlope = rightSlopeTotal/rightAmount
+	avgLeftYint = leftYintTotal/leftAmount
+	avgRightYint = rightYintTotal/rightAmount
+
+	leftYpoint1 = imshape[0]
+	leftXpoint1 = int((leftYpoint1-avgLeftYint)/avgLeftSlope)
+
+	leftYpoint2 = 310
+	leftXpoint2 = int((leftYpoint2-avgLeftYint)/avgLeftSlope)
+
+	cv2.line(line_image,(leftXpoint1,leftYpoint1),(leftXpoint2,leftYpoint2),(255,0,0),10) 
+
+	rightYpoint1 = imshape[0]
+	rightXpoint1 = int((rightYpoint1-avgRightYint)/avgRightSlope)
+
+	rightYpoint2 = 310
+	rightXpoint2 = int((rightYpoint2-avgRightYint)/avgRightSlope)
+	cv2.line(line_image,(rightXpoint1,rightYpoint1),(rightXpoint2,rightYpoint2),(0,0,255),10) 
+
 
 	# Create a "color" binary image to combine with line image
-	color_edges = np.dstack((masked_edges, masked_edges, masked_edges)) 
+	# color_edges = np.dstack((masked_edges, masked_edges, masked_edges)) 
 
 	# Draw the lines on the edge image
-	# lines_edges = cv2.addWeighted(image, 0.8, line_image, 1, 0)
-	lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
+	lines_edges = cv2.addWeighted(image, 0.8, line_image, 1, 0)
+	# lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
 
 
 	return lines_edges
 
 
 
-i = 0
-inputImg=''
+# i = 0
+# inputImg=''
 
-while(i<6):
-	if(i==0):
-		inputImg = 'solidWhiteCurve'
-	elif(i==1):
-		inputImg = 'solidWhiteRight'
-	elif(i==2):
-		inputImg = 'solidYellowCurve'
-	elif(i==3):
-		inputImg = 'solidYellowCurve2'
-	elif(i==4):
-		inputImg = 'solidYellowLeft'
-	elif(i==5):
-		inputImg = 'whiteCarLaneSwitch'
-	#reading in an image
-	inputPath = 'CarND-LaneLines-P1/test_images/'+inputImg+'.jpg'
-	outputPath = 'CarND-LaneLines-P1/test_images/'+inputImg + 'Output2'+'.jpg'
-	image = mpimg.imread(inputPath)
-	outputImage = process_image(image)
-	mpimg.imsave(outputPath,outputImage)
-	i = i +1
-
-
+# while(i<6):
+# 	if(i==0):
+# 		inputImg = 'solidWhiteCurve'
+# 	elif(i==1):
+# 		inputImg = 'solidWhiteRight'
+# 	elif(i==2):
+# 		inputImg = 'solidYellowCurve'
+# 	elif(i==3):
+# 		inputImg = 'solidYellowCurve2'
+# 	elif(i==4):
+# 		inputImg = 'solidYellowLeft'
+# 	elif(i==5):
+# 		inputImg = 'whiteCarLaneSwitch'
+# 	#reading in an image
+# 	inputPath = 'CarND-LaneLines-P1/test_images/'+inputImg+'.jpg'
+# 	outputPath = 'CarND-LaneLines-P1/test_images/'+inputImg + 'Output2'+'.jpg'
+# 	image = mpimg.imread(inputPath)
+# 	outputImage = process_image(image)
+# 	mpimg.imsave(outputPath,outputImage)
+# 	i = i +1
 
 
-# # Import everything needed to edit/save/watch video clips
-# from moviepy.editor import VideoFileClip
-# from IPython.display import HTML
 
 
-# inputVideo = 'solidWhiteRight'
-# video_output = inputVideo + 'output.mp4'
-# inputVideoPath = "CarND-LaneLines-P1/"+inputVideo+".mp4"
-# clip1 = VideoFileClip(inputVideoPath)
-# white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
-# white_clip.write_videofile(video_output, audio=False)
+# Import everything needed to edit/save/watch video clips
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+
+inputVideo = 'solidYellowLeft'
+video_output = inputVideo + 'output.mp4'
+inputVideoPath = "CarND-LaneLines-P1/"+inputVideo+".mp4"
+clip1 = VideoFileClip(inputVideoPath)
+white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+white_clip.write_videofile(video_output, audio=False)
